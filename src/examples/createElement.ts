85708c21bf9fcc873ba118a4337c700ad5a29e32
kurtuy4564 <kurtuy4564@mail.ru>
2026-09-23 09:13:40 +0300

    feat: implement createElement

---
+++ src/examples/createElement.ts
@@ -0,0 +1,25 @@
+import type { ChildrenType, VNodeRender, VNodeType } from './types'
+
+export function createElement(
+  vNodeType: VNodeType,
+  props: Record<string, unknown> | null = {},
+  ...children: ChildrenType
+): VNodeRender {
+  return {
+    type: vNodeType || 'div',
+    props: props ?? {},
+    children: flatten(children),
+  }
+}
+
+function flatten(children: ChildrenType): ChildrenType {
+  const result: ChildrenType = []
+  for (const child of children) {
+    if (Array.isArray(child)) {
+      result.push(...flatten(child))
+    } else {
+      result.push(child)
+    }
+  }
+  return result
+}
